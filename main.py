a02768a53228fe1b949198c4023c5203da52ebd7
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-18 00:02:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def home():
 @app.post("/ask")
 def ask():
     return {"response": "Halo, aku NOVA!"}
+    
+#Hanya jalankan server jika di lokal
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
